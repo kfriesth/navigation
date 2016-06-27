02cb7a5f109035102de9613e03b2a744f0bc2e4d
Jason Mercer <jmercer@clearpathrobotics.com>
2016-06-27 08:10:47 -0400

    Treat 2pi periodicity properly in yaw tolerance decisions.

--- nav_core/include/nav_core/goal_tolerances.h
+++ nav_core/include/nav_core/goal_tolerances.h
@@ -135,8 +135,13 @@ public:
    */
   bool lessThanTolerances(float dx, float dy, float dyaw) const
   {
+    dyaw = fmod(fmod(dyaw, 2.0*M_PI) + 2.0*M_PI, 2.0*M_PI);
+    if (dyaw > M_PI)
+    {
+      dyaw -= 2.0 * M_PI;
+    }
     return (dx * dx + dy * dy <= goal_tolerance_xy_ * goal_tolerance_xy_) &
-           (fmod(fabs(dyaw), 2.0 * M_PI) <= goal_tolerance_yaw_);
+           (dyaw <= goal_tolerance_yaw_);
   }
 
   /**
